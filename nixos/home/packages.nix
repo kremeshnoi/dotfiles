4760b9602cf9c041d9b6efe640f9df7834844c69
kremeshnoi <alexander.kremeshnoi@gmail.com>
2026-08-06 21:22:44 +0300

    feat

--- nixos/home/packages.nix
+++ nixos/home/packages.nix
@@ -2,46 +2,13 @@
 
 {
   home.packages = with pkgs; [
-    bandwhich
-    btop
     dive
-    dua
-    fastfetch
     glow
     herdr
     k9s
-    lazydocker
-    rainfrog
-    superfile
-    watchexec
-    xh
-
-    bat
-    eza
-    fd
-    ripgrep
-    jq
-    tree
-    dos2unix
-    unzip
-    zip
-    wget
-    curl
-    sqlite
-    yt-dlp
-
     gh
-    git-lfs
-    lazygit
     neovim
 
-    ngrok
-    railway
-    uv
-
-    cargo-llvm-cov
-    cargo-watch
-    dprint
     eslint_d
     prettier
     shfmt
